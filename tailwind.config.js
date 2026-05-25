bfef757f86f8b4ee0f29cdd4326a0cf03eceeb3b
danielfmatheus <danielfmatheus@gmail.com>
2026-05-25 18:04:21 -0300

    Adicionando arquivos de configuracao do visual

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: {
+          900: "#001f3f",
+        },
+        gold: {
+          500: "#d4af37",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
